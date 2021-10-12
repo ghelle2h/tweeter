564171afc1e352ace5ecc7934d59f00f53dcd52e
Hamza Ghelle <hamzaghellegmail.com>
2021-10-12 14:15:16 -0400

    add comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  
+  //Count characters of tweet
   $('#tweet-text').on('input', function() {
    let maxChar = 140;
    console.log($(this).val())
    const inputData = $(this).val().length;
-  //  console.log(inputData);
+  
    const remainingChar = maxChar - inputData;
    const $counter = $('.formNewTweet').children('.submit').children('.counter');
   $counter.html(remainingChar); 
-  // console.log($counter);
+  // style remaining characters
   if(remainingChar < 0) {
     $counter.css('color', 'red');
   } else {
